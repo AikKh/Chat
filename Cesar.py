ab5e151cfc0bafd804a00a8aad9ea82a22b25730
Aik Khachatrian <aik.khachatrian@gmail.com>
2021-11-11 22:56:29 +0200

    fix Cesar

--- Cesar.py
+++ Cesar.py
@@ -1,6 +1,14 @@
 class Cesar():
 
-    alpha = 'abcdefghijklmnopqrstuvwxyz'
+    alpha = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz'
+    
+    def keySign(self, key):
+        if key > 0:
+            return 1
+        elif key < 0:
+            return -1
+        else:
+            return 0
 
     def encrypt(self, txt: str, key):
         res = ''
@@ -10,9 +18,12 @@ class Cesar():
                 res+=' '
                 continue
             
-            res += self.alpha[(self.alpha.find(i.lower()) + key) % len(self.alpha)]
+            res += self.alpha[(self.alpha.find(i) + (key + self.keySign(key))) % len(self.alpha)]
             
         return res
+
+    
+
 
 
 
--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Socket chat
 
 Comand Examples:
 1. '*-' - sent msg to all 
-2. 'nickname-' sent msg to specific person
+2. 'reg-' to registration
+3. 'nickname-' sent msg to specific person
 
 
